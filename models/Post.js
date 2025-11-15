e4cf2fa48f6b7e2f3fe63a0976aa3e31dc5be414
md-zihad <md.zihad0246@gmail.com>
2025-11-16 02:30:39 +0600

    feat(models/post): add Post mongoose schema with relations and validations
    - Add models/Post.js exporting Post model
    - Define fields:
      - title: String, trim, required, maxLength 100
      - body: String, required
      - author: ObjectId ref "User", required
      - tags: [String], required
      - thumbnail: String (optional)
      - readTime: String (optional)
      - likes, dislikes: arrays of ObjectId
      - comments: [ObjectId ref "Comment"]
    - Enable timestamps on the schema
    
    No breaking changes.

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 100,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tags: {
+      type: [String],
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+    },
+    readTime: {
+      type: String,
+    },
+    likes: [Schema.Types.ObjectId],
+    dislikes: [Schema.Types.ObjectId],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
