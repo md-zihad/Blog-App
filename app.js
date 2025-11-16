a07ea8524fec819db2c7376d814a07b575516463
md-zihad <md.zihad0246@gmail.com>
2025-11-16 18:43:04 +0600

    feat(auth): implement authentication routes and controllers for signup and login
    feat(middleware): add morgan and static file serving middleware
    chore(dependencies): update package.json to include morgan and ejs

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
-
+const morgan = require('morgan')
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const middlewares = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended: true}),
+    express.json()
+]
+
+app.use(middlewares)
 
 app.get('/', (req, res) => {
     res.json(
---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const signupGetController = (req, res, next) => {
+    
+}
+const signupPostController = (req, res, next) => {
+
+}
+const loginGetController = (req, res, next) => {
+
+}
+const loginPostController = (req, res, next) => {
+
+}
+const logoutController = (req, res, next) => {
+
+}
+
+module.exports = {
+    signupGetController, signupPostController, loginGetController, loginPostController, logoutController
+}
