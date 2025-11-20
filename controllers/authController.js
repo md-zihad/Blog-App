8a139b83d236bf6dd3300bd36ff296cd62aea4e0
md-zihad <md.zihad0246@gmail.com>
2025-11-20 15:23:34 +0600

    feat(auth): add authController with login handlers
    - loginPostController: verifies credentials with bcrypt.compare, returns 200 on success; returns invalid credential responses or 400 on exception

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,65 @@
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const signupGetController = async (req, res, next) => {
-    try {
-        const user = await User.find().select('email username')
-        if(!user){
-            return res.status(200).json(user)
-        }
-        res.status(200).json(user)
-    } catch (e) {
-        res.status(500)
+  try {
+    const user = await User.find().select("email username");
+    if (!user) {
+      return res.status(200).json(user);
     }
-}
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500);
+  }
+};
 const signupPostController = async (req, res, next) => {
-    const {username, email, password} = req.body
-    // console.log(req.body)
+  const { username, email, password } = req.body;
+  // console.log(req.body)
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    })
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    try {
-        let createdUser = await newUser.save()
-         res.status(201).json(createdUser)
-    } catch (e) {
-        return res.status(404).json('Error Occured')
+  try {
+    let createdUser = await newUser.save();
+    res.status(201).json(createdUser);
+  } catch (e) {
+    return res.status(404).json("Error Occured");
+  }
+};
+const loginGetController = (req, res, next) => {};
+
+const loginPostController = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      res.json("Invalid Credentials");
     }
-}
-const loginGetController = (req, res, next) => {
 
-}
-const loginPostController = (req, res, next) => {
+    let match = await bcrypt.compare(password, user.password);
 
-}
-const logoutController = (req, res, next) => {
-
-}
+    if (!match) {
+      res.json("Invalid Credentials");
+    }
+    res.status(200).json("Welcome, you're logged in");
+    //   console.log(user);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+const logoutController = (req, res, next) => {};
 
 module.exports = {
-    signupGetController, signupPostController, loginGetController, loginPostController, logoutController
-}
+  signupGetController,
+  signupPostController,
+  loginGetController,
+  loginPostController,
+  logoutController,
+};
