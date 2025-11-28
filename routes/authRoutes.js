ab1d968381bab5bb3b215ba273a4aaddb0c26cdb
md-zihad <md.zihad0246@gmail.com>
2025-11-29 03:29:19 +0600

    feat(validator): implement signup validation logic and remove inline validation from authRoutes
    No breaking changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { body } = require("express-validator");
-const User = require("../models/User");
+const signupValidator = require('../validators/auth/signupValidator')
+
 
 const {
   signupGetController,
@@ -10,33 +10,7 @@ const {
   logoutController,
 } = require("../controllers/authController");
 
-const signupValidator = [
-  body("username")
-    .isLength({ min: 3, max: 10 })
-    .withMessage(`username must be between 3 to 10 characters`)
-    .custom(async (username) => {
-      let user = await User.findOne({ username });
 
-      if (user) {
-        return Promise.reject("Username already taken");
-      }
-    })
-    .trim(),
-  body("email")
-    .isEmail()
-    .withMessage(`Please provide a valid Email`)
-    .custom(async (email) => {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return Promise.reject("Email already taken");
-      }
-    })
-    .normalizeEmail(),
-  body("password")
-    .isLength({ min: 5 })
-    .withMessage(`Password must be greater then 5 chars`),
-];
 
 router.get("/signup", signupGetController);
 router.post("/signup", signupValidator, signupPostController);
